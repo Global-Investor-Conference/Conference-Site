2fc74ea7f3e694db6def779ab561f6e19de6b284
Farzin <farzinadil@gmail.com>
2022-02-08 09:34:55 -0800

    Updated dates

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 05, 15, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2022, 05, 18, 8)}); // year, month, date, hour
         });
 });		
 
